42000b8fbda712a6e04b6a5ebbfc4451e1ca1d5e
박유진 <qkrdbw@naver.com>
2024-07-20 15:39:37 +0900

    install: axios

---
+++ src/components/main/Onions.tsx
@@ -0,0 +1,9 @@
+import { View } from 'react-native';
+
+const Onions = () => {
+    return (
+        <View>Onions</View>
+    )
+}
+
+export default Onions;
---
+++ src/screens/Main.tsx
@@ -0,0 +1,9 @@
+import { View } from "react-native"
+
+const Main = () => {
+    return (
+        <View>Main</View>
+    )
+};
+
+export default Main;
