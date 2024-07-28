b4ed485dc0361c0501318f6b630be62c50249b40
Minhyung Cho <dup33333@hufs.ac.kr>
2024-07-28 22:06:53 +0900

    style: sentReqs

--- src/components/dictionary/requestmodal/sentReq/sentReqItem.tsx
+++ src/components/dictionary/requestmodal/sentReq/sentReqItem.tsx
@@ -1,19 +1,34 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableWithoutFeedback, View } from "react-native";
 import { ReqTradeType } from "../../../../types/exchangeRequests";
+import { Button } from "../../../common/button";
 
 export const SentReqItem = ({ trade }: ReqTradeType) => {
   return (
-    <View style={styles.container}>
-      <Text>
-        사용자님의 {trade.req_onion}을 {trade.receiver.nickname}님의{" "}
-        {trade.res_onion}과 교환하기를 제안했어요
-      </Text>
-    </View>
+    <TouchableWithoutFeedback>
+      <View style={styles.container}>
+        <Text style={styles.text}>
+          사용자님의 {trade.req_onion}을 {trade.receiver.nickname}님의{" "}
+          {trade.res_onion}과 교환하기를 제안했어요
+        </Text>
+        <Button
+          background="lightgray"
+          onPress={() => {}}
+          text="취소"
+          width={50}
+        />
+      </View>
+    </TouchableWithoutFeedback>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: 10,
+  },
+  text: {
+    width: "80%",
   },
 });
