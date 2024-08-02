7a97be186e7f5f3eb045daf4671aafbb3583f43a
박유진 <qkrdbw@naver.com>
2024-08-02 21:50:13 +0900

    feat: Onions component

--- src/components/main/Onions.tsx
+++ src/components/main/Onions.tsx
@@ -1,11 +1,37 @@
-import { View, Text } from "react-native";
+import { View, Image, StyleSheet, Text } from "react-native";
+
+const positive1 = require("../../../assets/positive/1.png");
+const negative1 = require("../../../assets/negative/1.png");
 
 const Onions = () => {
   return (
-    <View>
-      <Text>Onions</Text>
+    <View style={styles.onionsStyle}>
+      <View style={styles.onion}>
+        <Image source={positive1} style={styles.onionStyle} />
+        <Text>긍정 양파</Text>
+      </View>
+      <View style={styles.onion}>
+        <Image source={negative1} style={styles.onionStyle} />
+        <Text>부정 양파</Text>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  onionsStyle: {
+    flexDirection: 'row',
+  },
+  onionStyle: {
+    marginBottom: 20,
+    width: 170,
+    height: 300,
+  },
+  onion: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 60,
+  }
+});
 
 export default Onions;
