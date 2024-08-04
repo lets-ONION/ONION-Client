105fa94e4ea4bf510143b69b02ae7b9cdd174c94
박유진 <qkrdbw@naver.com>
2024-08-04 13:58:03 +0900

    feat: main interface

---
+++ src/models/models.ts
@@ -0,0 +1,17 @@
+export interface Onion {
+    name: string;
+    level: number;
+    image_url: string;
+}
+
+export interface DataResponse {
+    pos_onion: Onion;
+    neg_onion: Onion;
+    is_spoken: boolean;
+}
+
+export interface FetchMainResponse {
+    msg: string;
+    code: number;
+    data: DataResponse;
+}
