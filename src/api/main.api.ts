2f270052b96cecb8794c01ac4d4071d4270e8703
박유진 <qkrdbw@naver.com>
2024-08-04 13:57:29 +0900

    feat: main api

---
+++ src/api/main.api.ts
@@ -0,0 +1,31 @@
+import { httpClient } from "./http";
+
+export const fetchMain = async () => {
+    const response = await httpClient.get(`/main`);
+    return response.data;
+};
+
+export const notePositive = async (note: string) => {
+    const response = await httpClient.post(`/main/positive/note`, { note });
+    return response.data;
+};
+
+export const waterPositive = async () => {
+    const response = await httpClient.patch(`/main/positive/water`);
+    return response.data;
+};
+
+export const waterNegative = async () => {
+    const response = await httpClient.patch(`/main/negative/water`);
+    return response.data;
+};
+
+export const checkPositive = async (note: string) => {
+    const response = await httpClient.post(`/main/check/pos`, { note });
+    return response.data;
+};
+
+export const checkNegative = async (note: string) => {
+    const response = await httpClient.post(`/main/check/neg`, { note });
+    return response.data;
+};
